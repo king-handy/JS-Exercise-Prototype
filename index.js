b4a8683487db396141f070785d055e37c392612c
king-handy <a.king.life@pm.me>
2021-03-15 23:24:39 -0400

    Task2

--- index.js
+++ index.js
@@ -73,8 +73,15 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+
+  Car.prototype.fill = function() {
+    tank++
   }
   
   
